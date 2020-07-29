3ad022e3ce4db26c5c9db035164686ac09304145
gregorymcintyre <gregmcintyre1984@gmail.com>
2020-07-29 21:47:24 +1000

    UPDATE: Credit Task Submission

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # iot-data-SIT314
 SIT314 Iot Data Tasks
+
+sudo apt-get install nodejs npm mongoose plotly
+
+mongoDB
+plot.ly
--- sit314_task4/iotsensor.js
+++ sit314_task4/iotsensor.js
@@ -1,37 +1,63 @@
 const mongoose = require('mongoose');
+var plotly = require('plotly')("gmcintyre", "tsHRoEapXm8pUhQprdlw")
 
-//mongoose.connect('mongodb+srv://greg:RRz9GRjRLp37hz3@sit209-wt5gz.mongodb.net/data');
-mongoose.connect('mongodb+srv://greg:RRz9GRjRLp37hz3@sit314.bsjyx.mongodb.net/data');
-//mongodb+srv://greg:RRz9GRjRLp37hz3@sit314.bsjyx.mongodb.net/<dbname>
+var data = {
+	x: [],
+	y: [],
+	type: "scatter"
+};
 
-const Sensor = require('./models/sensor');
+setInterval(sensortest, 10000);
 
-const sensordata = {
-	id: 0,
-	name: "temperaturesensor",
-	address: "221 Burwood Hwy, Burwood VIC 3125",
-	time: Date.now(),
-	temperature: 20
-}
+function sensortest(){
+	time = Date.now();
+	mongoose.connect('mongodb+srv://greg:RRz9GRjRLp37hz3@sit314.bsjyx.mongodb.net/data');
 
-const low = 10;
-const high = 40;
-reading = Math.floor(Math.random() * (high - low) + low);
-sensordata.temperature = reading;
+	const Sensor = require('./models/sensor');
 
-const jsonString = JSON.stringify(sensordata);
-console.log(jsonString);
+	const sensordata = {
+		id: 0,
+		name: "temperaturesensor",
+		address: "221 Burwood Hwy, Burwood VIC 3125",
+		time: Date.now(),
+		temperature: 20
+	}
 
-const newSensor = new Sensor({
-	id: sensordata.id,
-	name: sensordata.name,
-	address: sensordata.address,
-	time: sensordata.time,
-	temperature: sensordata.temperature
-});
+	const low = 10;
+	const high = 40;
+	reading = Math.floor(Math.random() * (high - low) + low);
+	sensordata.temperature = reading;
 
-newSensor.save().then(doc => {
-	console.log(doc);
+	const jsonString = JSON.stringify(sensordata);
+	//console.log(jsonString);
+
+	const newSensor = new Sensor({
+		id: sensordata.id,
+		name: sensordata.name,
+		address: sensordata.address,
+		time: sensordata.time,
+		temperature: sensordata.temperature
+	});
+
+
+
+	newSensor.save().then(doc => {
+		time=Date.now()-time;
+		//console.log("Runtime: " + (Date.now()-time) + "ms");
+		console.log("Runtime: " + time + "ms");
+		console.log(doc);
+		
+		data.x.push((new Date()).toISOString());
+		data.y.push(time);
+		
+		var graphOptions = {filename: "iot-performance", fileopt:"overwrite"};
+		plotly.plot(data, graphOptions, function (err, msg) {
+	
+		if (err) return console.log(err);
+			console.log(msg);
+		});
+		
 	}).then(() => {
-	mongoose.connection.close();
-});
+		mongoose.connection.close();
+	});
+}
